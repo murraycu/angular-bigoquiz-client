da3a4eb5552a626854e582020f649451dd2a1e0b
Murray Cumming <murrayc@murrayc.com>
2017-07-21 09:57:35 +0200

    CSS: Move History CSS into specific file.

--- src/app/history.component.ts
+++ src/app/history.component.ts
@@ -4,6 +4,7 @@ import { OnInit } from '@angular/core';
 @Component({
   selector: 'history',
   templateUrl: './history.component.html',
+  styleUrls: ['./history.component.css']
 })
 export class HistoryComponent {
 
