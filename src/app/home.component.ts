7f23998892d114bd557f049e00a2981e4bc97d3c
Murray Cumming <murrayc@murrayc.com>
2017-07-21 09:46:30 +0200

    CSS: Move Home CSS into specific file.

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -4,6 +4,7 @@ import { OnInit } from '@angular/core';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   ngOnInit(): void {
