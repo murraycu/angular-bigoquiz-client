51dbfc351eaf30074d20832d23231327aa0c1f8e
Murray Cumming <murrayc@murrayc.com>
2017-07-24 10:39:01 +0200

    UserStats: Check that the UserQuestionHistory is defined.

--- src/app/data-structure/user-stats.spec.ts
+++ src/app/data-structure/user-stats.spec.ts
@@ -1,9 +1,11 @@
 import { UserStats } from './user-stats';
 import { QuizQuestion } from './quiz-question';
+import { QuizText } from './quiz-text';
 
 describe('UserStats without the TestBed', () => {
   let userStats: UserStats;
   let QUESTION_ID1: string;
+  let QUESTION_TEXT1: string;
   let question1: QuizQuestion;
 
   beforeEach(() => {
@@ -11,8 +13,11 @@ describe('UserStats without the TestBed', () => {
 
     // TODO: Use a createQuestion() utility function instead.
     QUESTION_ID1 = "questionid1";
+    QUESTION_TEXT1 = "questiontitle1";
     question1 = new QuizQuestion();
     question1.id = QUESTION_ID1;
+    question1.text = new QuizText();
+    question1.text.text = QUESTION_TEXT1;
   });
 
   it ('answered should default to 0.', () => {
@@ -49,12 +54,18 @@ describe('UserStats without the TestBed', () => {
     userStats.updateProblemQuestion(question1, false);
 
     expect(userStats.questionHistories.size).toBe(1);
+    expect(userStats.questionHistories.get(QUESTION_ID1)).toBeTruthy();
+    expect(userStats.questionHistories.get(QUESTION_ID1).questionId).toBeTruthy();
+    expect(userStats.questionHistories.get(QUESTION_ID1).questionTitle).toBeTruthy();
   });
 
   it ('updateProblemQuestion(, true) fills questionHistories.', () => {
     userStats.updateProblemQuestion(question1, true);
 
     expect(userStats.questionHistories.size).toBe(1);
+    expect(userStats.questionHistories.get(QUESTION_ID1)).toBeTruthy();
+    expect(userStats.questionHistories.get(QUESTION_ID1).questionId).toBeTruthy();
+    expect(userStats.questionHistories.get(QUESTION_ID1).questionTitle).toBeTruthy();
   });
 
   it ('updateProblemQuestion(, false) creates topProblemquestionHistories.', () => {
