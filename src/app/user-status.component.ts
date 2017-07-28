6c70645716028d1b66ce18bfd24ef338b17c3d18
Murray Cumming <murrayc@murrayc.com>
2017-07-28 17:17:20 +0200

    Revert use of Google Sign In
    This is not the same as the Users API. It is an alternative.
    We might use this again when the server uses Google Sign In,
    which seems to be a better choice when implementing multiple sign in
    methods, such as Facebook too.

--- src/app/user-status.component.ts
+++ src/app/user-status.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, Component, OnInit, NgZone, Input } from '@angular/core';
+import { Component, OnInit, NgZone, Input } from '@angular/core';
 
 import { UserService } from './rest-api-clients/user.service';
 import { LoginInfo } from './data-structure/login-info';
@@ -11,15 +11,12 @@ declare var gapi: any;
   templateUrl: './user-status.component.html',
   styleUrls: ['./user-status.component.css']
 })
-export class UserStatusComponent implements OnInit, AfterViewChecked {
+export class UserStatusComponent implements OnInit {
   // We don't want to show the logout button in the header,
   // but we do want to show it on the user/profile page.
   @Input() showLogOutWhenAppropriate: boolean;
 
   loginInfo: LoginInfo;
-
-  googleLoginButtonId = 'google-login-button';
-  private loginButtonRendered = false;
 
   constructor(private userService: UserService, private zone: NgZone) { }
 
@@ -30,52 +27,5 @@ export class UserStatusComponent implements OnInit, AfterViewChecked {
     const currentUrl: string = window.location.href;
 
     this.userService.getUser(currentUrl).then(loginInfo => this.loginInfo = loginInfo);
-  }
-
-  // Other people seem to use ngAfterViewInit(), but the <div> does not exist yet at that point.
-  // https://stackoverflow.com/questions/35530483/google-sign-in-for-websites-and-angular-2-using-typescript/42802835
-  ngAfterViewChecked() {
-    // console.log('debug ngAfterViewChecked');
-
-    if (!document.getElementById(this.googleLoginButtonId)) {
-      // console.log('debug ngAfterViewChecked: div does not exist.');
-      return;
-    }
-
-    // Avoid an infinite loop.
-    // gapi.signin2.render() seems to trigger another ngAfterViewChecked().
-    if (this.loginButtonRendered) {
-      return;
-    }
-
-    // console.log('debug ngAfterViewChecked: rendering button.');
-
-    this.loginButtonRendered = true;
-
-    // Converts the Google login button stub to an actual button.
-    gapi.signin2.render(
-      this.googleLoginButtonId,
-      {
-        'onSuccess': this.onGoogleLoginSuccess,
-        'scope': 'profile',
-        'theme': 'dark'
-      });
-  }
-
-  // A simple typescript method doesn't work: onGoogleLoginSuccess(loggedInUser): void {
-  // and we need to use an NgZone.
-  // See https://stackoverflow.com/questions/35530483/google-sign-in-for-websites-and-angular-2-using-typescript
-  onGoogleLoginSuccess = (loggedInUser) => {
-    this.zone.run(() => {
-      // Update the loginInfo locally,
-      // in case the login doesn't result in a complete page refresh.
-      if (!this.loginInfo) {
-        this.loginInfo = new LoginInfo();
-      }
-
-      this.loginInfo.userId = loggedInUser.userId;
-      this.loginInfo.nickname = loggedInUser.getBasicProfile().getName();
-      console.log(this);
-    });
   }
 }
