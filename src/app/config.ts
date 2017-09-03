b49e5dbeb1b3cf1b54889ae6d51fc8d71be51669
Murray Cumming <murrayc@murrayc.com>
2017-09-03 17:05:28 +0200

    Config: Use the beta (go) version of the REST API.

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export class Config {
   // When using gwt-bigoquiz with Jetty: static baseUrl: string = 'http://localhost:8080';
-  static baseUrl: string = 'http://bigoquiz.com';
+  static baseUrl: string = 'http://betaapi.bigoquiz.com';
 }
