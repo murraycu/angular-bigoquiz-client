9351d20f40eb35608ee24a35945aa98a970638c5
Murray Cumming <murrayc@murrayc.com>
2018-01-31 20:55:10 +0100

    Move betaapi.bigoquiz.com to api.bigoquiz.com

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export class Config {
   // When using gwt-bigoquiz with Jetty: static baseApiUrl: string = 'http://localhost:8080';
-  static baseApiUrl = 'https://betaapi.bigoquiz.com';
+  static baseApiUrl = 'https://api.bigoquiz.com';
 
   // TODO: Disover this dynamically?
   static baseUrl = 'https://beta.bigoquiz.com';
