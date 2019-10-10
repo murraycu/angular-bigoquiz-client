e9205f736cb22ca4003ffbd9212cf88d81129000
Murray Cumming <murrayc@murrayc.com>
2019-10-10 07:48:55 +0200

    tests: UserHistorySections: Add an extra test

--- src/app/data-structure/user-history-sections.spec.ts
+++ src/app/data-structure/user-history-sections.spec.ts
@@ -4,7 +4,7 @@ import { UserStats } from "./user-stats";
 
 describe("UserHistorySections without the TestBed", () => {
 
-  it("getUserStatsForSection() should return value previously set with setUserStatsForSection().", () => {
+  it("getUserStatsForSection() should return value previously added with setUserStatsForSection().", () => {
     const sections: UserHistorySections = new UserHistorySections();
 
     const sectionId = "some-section-id";
@@ -21,5 +21,33 @@ describe("UserHistorySections without the TestBed", () => {
     expect(result).toBeTruthy();
     expect(result.sectionId).toBe(sectionId);
     expect(result.sectionTitle).toBe(sectionTitle);
+  });
+
+  it("getUserStatsForSection() should return value previously changed with setUserStatsForSection().", () => {
+    const sections: UserHistorySections = new UserHistorySections();
+
+    const sectionId = "some-section-id";
+    const sectionTitle = "some-section-title";
+    const changedSectionTitle = "changed-section-title";
+
+    const userStats: UserStats = new UserStats();
+    userStats.sectionId = sectionId;
+    userStats.sectionTitle = sectionTitle;
+    sections.setUserStatsForSection(sectionId, userStats);
+
+    const userStats2 = sections.getUserStatsForSection(sectionId);
+
+    // Change it:
+    userStats2.sectionTitle = changedSectionTitle;
+
+    // We don't need to call setUserStatsForSection()
+    // because we are already changing a shared reference.
+    // sections.setUserStatsForSection(sectionId, userStats2)
+
+    const result = sections.getUserStatsForSection(sectionId);
+
+    expect(result).toBeTruthy();
+    expect(result.sectionId).toBe(sectionId);
+    expect(result.sectionTitle).toBe(changedSectionTitle);
   });
 });
