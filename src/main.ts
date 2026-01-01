b1b9763c6416a4f1ff3bde8d11383d89f468deb3
Murray Cumming <murrayc@murrayc.com>
2026-01-01 12:57:21 +0100

    Revert "app.module: Don't use (now standalone) AppComponent in boostrap"
    This reverts commit 5c4a11d8414a39efe7cbb05244f66424f3b38e2d.
    
    So we can revert the subsequent "Update to use Angular standalone
    components" commit.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import 'reflect-metadata';
-import {enableProdMode, importProvidersFrom} from '@angular/core';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import {bootstrapApplication} from "@angular/platform-browser";
-import {AppComponent} from "./app/app.component";
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(AppModule)
-  ]
-}).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
